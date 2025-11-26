1aabc209a6f93a6afb4f763a8f8cb1f063c5e79d
Ninh Pham <ninhninh2906@gmail.com>
2025-11-26 19:42:01 +0700

    Nhung bootstrap va file tinh

--- index.js
+++ index.js
@@ -6,8 +6,10 @@ const route = require("./routes/client/index.route");
 const app = express();
 const port = process.env.PORT;
 
-app.set('views', './views')
-app.set('view engine', 'pug')
+app.set('views', './views');
+app.set('view engine', 'pug');
+
+app.use(express.static("public"));
 
 //Routes
 route(app); 
