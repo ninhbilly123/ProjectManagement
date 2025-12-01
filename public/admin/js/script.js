a8f05d63b4be1a7a03dff2f3540a6e5917e38f1d
Ninh Pham <ninhninh2906@gmail.com>
2025-12-02 04:05:15 +0700

    Tinh nang tim kiem (FrontEnd)

--- public/admin/js/script.js
+++ public/admin/js/script.js
@@ -2,7 +2,6 @@
 const buttonsStatus = document.querySelectorAll("[button-status]"); 
 if (buttonsStatus.length > 0) {
   let url = new URL(window.location.href);
-  console.log(url);
 
   buttonsStatus.forEach(button => {
     button.addEventListener("click", () => {
@@ -14,11 +13,32 @@ if (buttonsStatus.length > 0) {
       else {
         url.searchParams.delete("status");
       }
-      console.log(url.href);
         
-      window.location.href = url;
+      window.location.href = url.href;
     });
   });
 }
 
 // End Button Status
+
+// Form Search
+const formSearch = document.querySelector("#form-search"); 
+if (formSearch) {
+  let url = new URL(window.location.href);
+  formSearch.addEventListener("submit", (e) => {
+    e.preventDefault(); 
+    const keyword = e.target.elements.keyword.value
+    
+    if (keyword) {
+      url.searchParams.set("keyword", keyword);
+    }
+    else {
+      url.searchParams.delete("keyword"); 
+    }
+
+    window.location.href = url.href;
+  })
+}
+
+// End Form Search
+
