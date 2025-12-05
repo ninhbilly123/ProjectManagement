81cff6dbb3b59569f0c79ca0305c07a85a38fb20
Ninh Pham <ninhninh2906@gmail.com>
2025-12-05 12:20:05 +0700

    Toi uu phan bo loc

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -1,33 +1,12 @@
 const Product = require("../../models/product.model"); 
 
+const filterStatusHelper = require("../../helpers/filterStatus"); 
+
 // [GET] /admin/products
 module.exports.index = async (req, res) => {
-  let filterStatus = [
-    {
-      name: "Tất cả",
-      status: "",
-      class: ""
-    },
-    {
-      name: "Hoạt động",
-      status: "active",
-      class: ""
-    },
-    {
-      name: "Dừng hoạt động",
-      status: "inactive",
-      class: ""
-    }
-  ]
-
-  if (req.query.status) {
-    const index = filterStatus.findIndex(item => item.status == req.query.status); 
-    filterStatus[index].class = "active"; 
-  }
-  else {
-    const index = filterStatus.findIndex(item => item.status == ""); 
-    filterStatus[index].class = "active"; 
-  }
+  
+  //Đoạn bộ lọc
+  const filterStatus = filterStatusHelper(req.query); 
 
   let find = {
     deleted: false,
@@ -48,7 +27,6 @@ module.exports.index = async (req, res) => {
 
   const products = await Product.find(find);
 
-  // console.log(products); 
 
   res.render("admin/pages/products/index", {
     pageTitle: "Danh sách sản phẩm",
