27f396c89c5293870f9685a903157243d6198913
Ninh Pham <ninhninh2906@gmail.com>
2025-12-13 23:13:14 +0700

    Xoa mem

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -86,7 +86,12 @@ module.exports.changeMulti = async (req, res) => {
 module.exports.deleteItem = async (req, res) => {
   const id = req.params.id; 
 
-  await Product.deleteOne({ _id: id });
+  // await Product.deleteOne({ _id: id });
+  await Product.updateOne({ _id: id }, {
+    deleted: true,
+    deletedAt: new Date()
+  }); 
+
 
   // res.redirect("back")
   const referer = req.get("Referer");
