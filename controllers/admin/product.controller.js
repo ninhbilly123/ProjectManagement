b30231d4aecb136c81ad75808be288fa82e6d490
Ninh Pham <ninhninh2906@gmail.com>
2025-12-14 03:06:12 +0700

    Hien thi thong bao sau khi doi vi tri

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -99,6 +99,8 @@ module.exports.changeMulti = async (req, res) => {
         await Product.updateOne({ _id: id}, { 
           position: position 
         }); 
+
+        req.flash("success", `Đã đổi vị trí thành công ${ids.length} sản phẩm!`);
       }
     default: 
       break; 
